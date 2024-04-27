66067ccdf007dabe6391912f43df6298667334c1
Bivek Gharti <gcbibek3353@gmail.com>
2024-04-27 20:22:52 +0530

    day17_solved

--- day17.js
+++ day17.js
@@ -2,7 +2,18 @@
 // example: console.log(toCamelCase("hello world thapa")); output: helloWorldThapa
 
 const toCamelCase = str =>{
-
+console.log(str.split(" ")[0]);
+arr = str.split(" ")
+str1 = ""
+arr.forEach((element,index) => {
+    if(index==0){
+        str1+=element.toLowerCase();
+    }
+    else{
+        str1+=element.charAt(0).toUpperCase() + element.slice(1)
+    }
+});
+return str1;
 
 }
-toCamelCase("hello world")
+console.log(toCamelCase("hello world By bivek"));
